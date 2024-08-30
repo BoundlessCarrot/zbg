e7b2ca93ff03f6fdf0ce6ae60ed6306b97edb5ba
Jordan S <jordan.streete@gmail.com>
2024-08-30 23:59:24 +0200

    update a bunch, types should be more matching now

--- zbg.zig
+++ zbg.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 
-const Board = struct {
+pub const Board = struct {
     points: [26]Point,
     bar: [2]u8,
     bearOff: [2]u8,
@@ -8,21 +8,36 @@ const Board = struct {
     const Self = @This();
 
     pub fn init() Board {
-        return Board{ .points = [_]Point{Point{ .player = undefined, .count = 0 }} ** 26, .bar = [2]u8{ 0, 0 }, .bearOff = [2]u8{ 0, 0 } };
+        const points = [_]Point{Point{ .player = undefined, .count = 0 }} ** 26;
+        return Board{ .points = points, .bar = [2]u8{ 0, 0 }, .bearOff = [2]u8{ 0, 0 } };
     }
 
-    pub fn movePiece(self: Self, from: u8, to: u8) void {
+    // TODO: Needs test case(s)
+    pub fn movePiece(self: *Self, from: u8, to: u8) void {
         self.points[from].count -= 1;
         self.points[to].count += 1;
+    }
+
+    // Set the bpard for various game types. Will be expanded later
+    pub fn dressBoard(self: *Self, players: [2]Player) void {
+        self.points[1] = Point{ .player = players[0], .count = 2 };
+        self.points[12] = Point{ .player = players[0], .count = 5 };
+        self.points[17] = Point{ .player = players[0], .count = 3 };
+        self.points[19] = Point{ .player = players[0], .count = 5 };
+
+        self.points[6] = Point{ .player = players[1], .count = 5 };
+        self.points[8] = Point{ .player = players[1], .count = 3 };
+        self.points[13] = Point{ .player = players[1], .count = 5 };
+        self.points[24] = Point{ .player = players[1], .count = 2 };
     }
 };
 
 const Point = struct {
-    player: u8,
+    player: Player,
     count: u8,
 };
 
-const Player = struct {
+pub const Player = struct {
     name: []const u8,
     color: usize,
     score: usize,
@@ -34,7 +49,7 @@ const Player = struct {
     }
 };
 
-const GameState = struct {
+pub const GameState = struct {
     board: Board,
     players: [2]Player,
     currentPlayer: Player,
@@ -42,12 +57,36 @@ const GameState = struct {
 
     const Self = @This();
 
-    pub fn init() GameState {
-        return GameState{ .board = Board.init(), .players = [2]Player{ Player.init("Player 1", 0), Player.init("Player 2", 1) }, .currentPlayer = 0, .dice = [2]u8{ undefined, undefined } };
+    pub fn init(name0: []const u8, name1: []const u8) GameState {
+        const players = [2]Player{ Player.init(name0, 0), Player.init(name1, 1) };
+        const board = Board.init();
+        board.dressBoard(players);
+
+        return GameState{
+            .board = board,
+            .players = players,
+            .currentPlayer = players[0],
+            .dice = [2]u8{ undefined, undefined },
+        };
+    }
+
+    // roll 2 dice
+    // TODO: Needs test case(s)
+    pub fn rollDice(self: *Self) void {
+        var prng = std.rand.DefaultPrng.init(blk: {
+            var seed: u64 = undefined;
+            try std.os.getrandom(std.mem.asBytes(&seed));
+            break :blk seed;
+        });
+        const rand = prng.random();
+
+        self.dice = .{ rand.intRangeAtMost(u8, 1, 6), rand.intRangeAtMost(u8, 1, 6) };
     }
 };
 
-const RulesEngine = struct {
+pub const RulesEngine = struct {
+    // determine whether a move is valid by checking whether the specific move is in the moves hashmap for the entire board
+    // TODO: Needs test case(s)
     pub fn isValidMove(from: Point, to: Point, validMovesHash: std.AutoHashMap(Point, []Point)) bool {
         if (validMovesHash.contains(from)) {
             const moveList = validMovesHash.get(from);
@@ -59,10 +98,12 @@ const RulesEngine = struct {
         return false;
     }
 
+    // check whether the game is over if player pieces are off the board
     pub fn isGameOver(gameState: *GameState) bool {
         return (gameState.board.points[25].count == 15 or gameState.board.points[0].count == 15);
     }
 
+    // which player won
     pub fn getWinner(gameState: *GameState) Player {
         var player: Player = undefined;
         if (gameState.board.points[0].count == 15) {
@@ -73,7 +114,9 @@ const RulesEngine = struct {
         return player;
     }
 
-    pub fn getAllValidMoves(gameState: *GameState, allocator: std.mem.Allocator) std.AutoHashmap(Point, []Point) {
+    // create a hashmap with all possible moves for all points owned by the current player
+    // TODO: Needs test case(s)
+    pub fn getAllValidMoves(gameState: *GameState, allocator: std.mem.Allocator) std.AutoHashMap(Point, []Point) {
         var moves = std.AutoHashMap(Point, [4]Point).init(allocator);
         defer moves.deinit();
 
@@ -85,8 +128,8 @@ const RulesEngine = struct {
                 var pointList = allocator.alloc(Point, 4);
                 defer allocator.free(pointList);
 
-                const jump_0 = gameState.board.points[i + gameState.dice[0]];
-                const jump_1 = gameState.board.points[i + gameState.dice[1]];
+                const jump_0 = internalGameBoard[i + gameState.dice[0]];
+                const jump_1 = internalGameBoard[i + gameState.dice[1]];
 
                 const jump_0_validator: bool = canMove(gameState.currentPlayer, jump_0);
                 const jump_1_validator: bool = canMove(gameState.currentPlayer, jump_1);
@@ -108,6 +151,7 @@ const RulesEngine = struct {
         return moves;
     }
 
+    // check whether move is basically valid
     fn canMove(player: Player, move: Point) bool {
         return move.player == player or move.count <= 1;
     }

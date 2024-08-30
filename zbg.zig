ca29a395008d1b020f81c6111e4b5de56d6462d7
Jordan S <jordan.streete@gmail.com>
2024-08-30 18:51:25 +0200

    beginnings of a working rules engine
    no doubles support but the rest looks suspiciously ok...

--- zbg.zig
+++ zbg.zig
@@ -8,7 +8,7 @@ const Board = struct {
     const Self = @This();
 
     pub fn init() Board {
-        return Board{ .points = [_]Point{Point{.player = undefined, .count = 0}} ** 26, .bar = [2]u8{ 0, 0 }, .bearOff = [2]u8{ 0, 0 } };
+        return Board{ .points = [_]Point{Point{ .player = undefined, .count = 0 }} ** 26, .bar = [2]u8{ 0, 0 }, .bearOff = [2]u8{ 0, 0 } };
     }
 
     pub fn movePiece(self: Self, from: u8, to: u8) void {
@@ -48,17 +48,19 @@ const GameState = struct {
 };
 
 const RulesEngine = struct {
-    // NOTE: Would it be better to do all rules that say you *can't* move or just generate all the valid moves?
-    pub fn isValidMove(gameState: *GameState, from: u8, to: u8) bool {
-        const from_point = gameState.board.points[from];
-        const to_point = gameState.board.points[to];
-        // if (from_point.player != gameState.currentPlayer) return false;
-        // if (to_point.player != gameState.currentPlayer && to.count > 1) return false;
-        // if (from + gameState.dice[0] != to && from + gameState.dice[1] != to) return false;
+    pub fn isValidMove(from: Point, to: Point, validMovesHash: std.AutoHashMap(Point, []Point)) bool {
+        if (validMovesHash.contains(from)) {
+            const moveList = validMovesHash.get(from);
+            if (std.mem.containsAtLeast(Point, moveList, 1, to)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     pub fn isGameOver(gameState: *GameState) bool {
-       return (gameState.board.points[25].count == 15 or gameState.board.points[0].count == 15); 
+        return (gameState.board.points[25].count == 15 or gameState.board.points[0].count == 15);
     }
 
     pub fn getWinner(gameState: *GameState) Player {
@@ -68,25 +70,39 @@ const RulesEngine = struct {
         } else if (gameState.board.points[24].count == 15) {
             player = gameState.players[1];
         }
-        return player; 
+        return player;
     }
 
-    pub fn getAllValidMoves(gameState: *GameState, allocator: std.mem.Allocator) []Point {
-        var moves = std.AutoHashMap(Point, []Point).init(allocator);
+    pub fn getAllValidMoves(gameState: *GameState, allocator: std.mem.Allocator) std.AutoHashmap(Point, []Point) {
+        var moves = std.AutoHashMap(Point, [4]Point).init(allocator);
         defer moves.deinit();
 
         const internalGameBoard: [26]Point = if (std.mem.eql(gameState.currentPlayer.name, "Player 1")) gameState.board.points else std.mem.reverse(Point, gameState.board.points);
-        
+
         for (internalGameBoard, 0..) |point, i| {
             if (point.player == gameState.currentPlayer) {
+                // NOTE: Doesn't include doubles
+                var pointList = allocator.alloc(Point, 4);
+                defer allocator.free(pointList);
+
                 const jump_0 = gameState.board.points[i + gameState.dice[0]];
                 const jump_1 = gameState.board.points[i + gameState.dice[1]];
 
-                if (canMove(gameState.currentPlayer, jump_0)) moves.put(point, jump_0);
-                if (canMove(gameState.currentPlayer, jump_1)) moves.put(point, jump_1);
+                const jump_0_validator: bool = canMove(gameState.currentPlayer, jump_0);
+                const jump_1_validator: bool = canMove(gameState.currentPlayer, jump_1);
 
-                const jump_2 = if (jump_0.player == gameState.currentPlayer) gameState.board.points[i + gameState.dice[0] + gameState.dice[0]] else undefined;
-                const jump_3 = if (jump_1.player == gameState.currentPlayer) gameState.board.points[i + gameState.dice[1] + gameState.dice[0]] else undefined;
+                if (jump_0_validator) {
+                    pointList[0] = jump_0;
+                    const jump_2 = internalGameBoard[i + gameState.dice[0] + gameState.dice[1]];
+                    if (canMove(gameState.currentPlayer, jump_2)) pointList[2] = jump_2;
+                }
+                if (jump_1_validator) {
+                    pointList[1] = jump_1;
+                    const jump_3 = internalGameBoard[i + gameState.dice[1] + gameState.dice[0]];
+                    if (canMove(gameState.currentPlayer, jump_3)) pointList[3] = jump_3;
+                }
+
+                moves.put(point, pointList);
             }
         }
         return moves;
